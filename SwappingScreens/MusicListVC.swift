28a18b42371b31c5f43bbc1b6ea2482c8f1727d7
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-05-28 12:45:09 -0500

    added segue from login to blue colored controller.

---
+++ SwappingScreens/MusicListVC.swift
@@ -0,0 +1,31 @@
+//
+//  MusicListVC.swift
+//  SwappingScreens
+//
+//  Created by Ricardo Herrera Petit on 5/28/17.
+//  Copyright © 2017 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+        
+    }
+
+  
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
