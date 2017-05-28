ca91f78f2ac4ce3061dccb665e73f1ffd70ccc88
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-05-28 13:07:47 -0500

    sending information to the third segue.

--- SwappingScreens/MusicListVC.swift
+++ SwappingScreens/MusicListVC.swift
@@ -9,26 +9,31 @@
 import UIKit
 
 class MusicListVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.blue
         
     }
-
-  
+    
+    
     @IBAction func backButtonPresed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func load3rdScreenPressed(_ sender: Any) {
+        let songTitle = "Quit Playing Games with My Heart"
+        performSegue(withIdentifier: "PlaySongVC", sender: songTitle)
     }
-    */
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? PlaySongVC {
+            
+            if let song = sender as? String {
+                destination.selectedSong = song
+                
+            }
+        }
+    }
+    
+    
 }
